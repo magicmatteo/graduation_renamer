c7ae0d3da6633db9cd827b3bccbd8abf575aa3d4
Matt <=>
2020-04-01 21:39:52 +1100

    Made script functional

--- main.py
+++ main.py
@@ -1,43 +1,59 @@
-import os, sys, shutil, time, string
+import os, sys, shutil
+from datetime import datetime
 
-try:
-	if sys.argv[1].lower() == '--help':
-		print(
-'''
-Ensure program is being run in root dir of photos. It will seach for 2017, 2018, 2019
-folders etc. 
-'''		)
-		sys.exit()
-except IndexError:
-	pass
 
-## Get date and set applicable years for folder names
-current_year = int(input("Please enter graduation year: "))
+### STATIC VARIABLES:
+current_year = datetime.now().year
+newdir = './renamed_photos/'
+### ------------------- ###
 
-allowed_folder_names = []
-for i in range(7):
-	allowed_folder_names.append(current_year - i)
+def help_check():
+	# Check for --help as arguement and display help text
+	try:
+		if sys.argv[1].lower() == '--help':
+			print(
+	'''
+	Ensure program is being run in root dir of photos. It will seach for 2017, 2018, 2019
+	folders etc. It will then put renamed photos into ./renamed_photos folder
+	'''		)
+			sys.exit()
+	except IndexError:
+		pass
+
+def folder_names(year_):
+	# Create a list of applicable folder names to store the photos in
+	result = []
+	for i in range(7):
+		result.append(year_ - i)
+	return result
 
 ## Remove spaces from names ie. Luca De Felize -> luca_de_felize
-def rename_file(fn):
+def rename_file(fn, year_):
 	intab = ' -'
 	outtab = '__'
-	fn = fn.rsplit('.')[0] + str(i) + ".jpg"
+	fn = fn.rsplit('.')[0] + str(year_) + ".jpg"
 	translation = fn.maketrans(intab, outtab)
 	return fn.translate(translation)
 
-## Rename Phase
-newdir = './renamed_photos/'
+def main_func():
+	# Main function that does the work. Renames all the files and puts them in folders
+	folders = folder_names(current_year)
+	for i in folders:
+		folder = str(i)
+		if os.path.exists(folder):
+			for subdir, dirs, files in os.walk(folder):
+				for file in files:
+					if file.lower() == '.ds_store':
+						continue
+					if not os.path.exists(newdir):
+						os.mkdir(newdir)
+					new_name = rename_file(file, folder)
+					sourcefile = os.path.realpath(os.path.join(folder,file))
+					shutil.copy(sourcefile, (newdir + new_name))
+					print(file + " renamed to " + new_name)
 
-for i in allowed_folder_names:
-	folder = str(i)
-	if os.path.exists(folder):
-		for subdir, dirs, files in os.walk(folder):
-			for file in files:
-				if not os.path.exists(newdir):
-					os.mkdir(newdir)
-				new_name = rename_file(file)
-				sourcefile = os.path.realpath(os.path.join(folder,file))
-				shutil.copy(sourcefile, (newdir + new_name))
-				print(file + " renamed to " + new_name)
+def main():
+	help_check()
+	main_func()
 
+main()
